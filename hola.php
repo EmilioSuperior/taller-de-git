b87371b5bb02b207c281ac7b1d37d7d0b67fc3e1
Emilio <elucher054@g.educaand.es>
2025-10-23 13:34:31 +0200

    hola usa la clase HolaMundo

--- hola.php
+++ hola.php
@@ -1,3 +1,9 @@
 <?php
 //hola
-echo "Hola Mundo\n";
+echo "Hola Mundo\n";<?php
+// Autor: Manuel Paniagua <mpaniagua@iestrassierra.com>
+// El nombre por defecto es Mundo
+require('HolaMundo.php');
+
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print new HolaMundo($nombre);
